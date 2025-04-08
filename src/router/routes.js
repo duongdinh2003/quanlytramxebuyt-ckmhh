fe04144c40d7258b00becf57f8377430760ef534
duongdinh2003 <lhdd1904@gmail.com>
2025-04-08 16:24:41 +0700

    config project

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,57 @@
 const routes = [
   {
-    path: '/',
+    path: '',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: '/map', component: () => import('pages/IndexPage.vue'), name: 'HomePage' },
+      {
+        path: 'user-management',
+        component: () => import('pages/UserManagementPage.vue'),
+        meta: {
+          authRequired: true,
+        },
+      },
+      {
+        path: 'projection-management',
+        component: () => import('pages/ProjectionManagementPage/index.vue'),
+        meta: {
+          authRequired: true,
+        },
+      },
+      {
+        path: 'location-management',
+        component: () => import('pages/locationManagementPage/index.vue'),
+        meta: {
+          authRequired: true,
+        },
+      },
+      { path: 'profile', component: () => import('pages/ProfilePage/index.vue') },
+      // { path: 'logout', component: () => import('pages/LogoutPage.vue') },
+    ],
+  },
+  {
+    path: '/login',
+    component: () => import('pages/LoginPage/index.vue'),
+    name: 'LoginPage',
+    meta: {
+      public: true,
+    },
+  },
+  {
+    path: '/register',
+    component: () => import('pages/LoginPage/RegisterPage.vue'),
+    name: 'RegisterPage',
+    meta: {
+      public: true,
+    },
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ]
 
 export default routes
